a2afeb70e16090481a21821cbc3251f084c25460
RafaelNixon <rafael.nixon.c@gmail.com>
2021-03-14 10:42:31 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>QR Code Scanner - Generator</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>QR Code</h2>
+            </div>
+            <div class="col-xs-6">
+                <h2>Scanner</h2>
+            </div>
+            <div class="col-xs-6">
+                <h2>Generator</h2>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
