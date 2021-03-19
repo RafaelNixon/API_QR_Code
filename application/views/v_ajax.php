c736dc7c0653013bf2134307f6139d9f4da00c50
RafaelNixon <62681554+RafaelNixon@users.noreply.github.com>
2021-03-19 13:10:43 +0700

    Add Code Igniter 3

---
+++ application/views/v_ajax.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+    <title>QR Code Scanner-Generator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/bootstrap.css'; ?>">
+    <script src="<?php echo base_url() . 'assets/js/jquery.min_3.5.1.js'; ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/bootstrap.min.js'; ?>"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>Ajax</h2>
+            </div>
+            <div class="col-xs-12">
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ application/views/v_main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <title>QR Code Scanner-Generator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/bootstrap.css'; ?>">
+    <script src="<?php echo base_url() . 'assets/js/jquery.min_3.5.1.js'; ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/bootstrap.min.js'; ?>"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>QR Code</h2>
+            </div>
+            <div class="col-xs-6">
+                <h2>Scanner</h2>
+            </div>
+            <div class="col-xs-6">
+                <h2>Generator</h2>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
